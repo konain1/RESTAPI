45984842ed5bfe00abed1f1626e1f1df2dcbaa0c
Konain1 <kaunainkasab@gmail.com>
2024-02-01 19:17:10 +0530

    route is not working

--- index.js
+++ index.js
@@ -1,87 +1,85 @@
-
-
-const express = require('express')
+const express = require('express');
 const app = express();
 
-let users = require('./mock_users.json')
+let users = require('./mock_users.json');
+const router = express.Router();
 
-// app.route('api/users/:id').get((req,res)=>{
-//     let id = Number(req.params.id)
-//     console.log(id)
-//     let user = users.filter((user)=>{
-//         return user.id = id
-//     })
+// router.route('/api/users/:id').get((req, res) => {
+//     let id = Number(req.params.id);
 
-//     res.json(user)
+//     try {
+//         if (id < 1 || users.length < id) {
+//             return res.status(401).json({ msg: 'invalid id' });
+//         }
 
+//         let user = users.filter(user => user.id === id);
+//         return res.json(user);
+//     } catch (error) {
+//         console.log('error is = ', error);
+//         return res.status(500).json({ msg: 'Internal Server Error' });
+//     }
+// }).delete((req, res) => {
+//     let id = Number(req.params.id);
 
-// })
-console.log(users.length)
+//     let NewList = users.filter((user) => user.id !== id);
+//     users = NewList;
 
+//     console.log(NewList);
+//     return res.json(NewList);
+// }).put((req, res) => {
+//     let id = Number(req.params.id);
+
+//     let myemail = 'newEmail@gmail.com';
+
+//     // Find the index of the user with the given id
+//     let index = users.findIndex(user => user.id === id);
+
+//     if (index !== -1) {
+//         // Update the email of the user at the found index
+//         users[index].email = myemail;
+//         return res.json(users[index]);
+//     }
+
+//     return res.status(404).json({ msg: `No user found with id = ${id}` });
+// });
 app.get('/api/users/:id',(req,res)=>{
 
-    let id = Number(req.params.id)
 
+    let id = Number(req.params.id);
 
-    if(id < 1 || users.length < id){
-       return res.status(401).json({msg:'invalid id'})
-    }
+        try {
+            if (id < 1 || users.length < id) {
+                return res.status(401).json({ msg: 'invalid id' });
+            }
     
-    let user = users.filter(user=> user.id === id)
-    return res.json(user)
-
-
+            let user = users.filter(user => user.id === id);
+            return res.json(user);
+        } catch (error) {
+            console.log('error is = ', error);
+            return res.status(500).json({ msg: 'Internal Server Error' });
+        }
 })
+app.use(express.json()); // Add this line to parse JSON in the request body
+app.use('/api/users', router);
 
-app.get('/api/users',(req,res)=>{
-    res.json(users)
-})
-
-app.post('/users',(req,res)=>{
-   let newUser = {
-    id:1001,
-    first_name:'konain',
-    email:'konain@gmail.com',
-    gender:'male',
-    company:'innovasor'
-    }
-
-    users.push(newUser)
-
-    return res.json(newUser)
-})
-app.delete('/users/:id',(req,res)=>{
-    let id =  Number(req.params.id)
-
-    let NewList = users.filter((user)=> user.id != id)
-    users = NewList
-
-    console.log(NewList)
-    return res.json(NewList)
-})
-
-
-
-// PUT
-    app.put('/users/:id', (req, res) => {
-        let id = Number(req.params.id);
-
-        let myemail = 'newEmail@gmail.com';
-
-        // Find the index of the user with the given id
-
-        let index = users.findIndex(user => user.id === id);
-        
-        
-        if (index !== -1) {
-        // Update the email of the user at the found index
-        users[index].email = myemail;
-        return res.json(users[index]);
-    }
-
-    return res.status(404).json({ msg: `No user found with id = ${id}` });
+app.get('/api/users', (req, res) => {
+    res.json(users);
 });
 
+app.post('/api/users', (req, res) => {
+    let newUser = {
+        id: users.length + 1,
+        first_name: 'konain',
+        email: 'konain@gmail.com',
+        gender: 'male',
+        company: 'innovasor'
+    };
 
+    users.push(newUser);
 
-app.listen(4005)
+    return res.json(newUser);
+});
+
+app.listen(4005, () => {
+    console.log('Server is running on http://localhost:4005');
+});
